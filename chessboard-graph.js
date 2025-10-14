8d1e71caac2d7b01f5c0d7b5af16112cf1836eeb
Michanoku <michael.werker@gmail.com>
2025-10-14 03:37:47 +0000

    Add file and initial logic comment

---
+++ chessboard-graph.js
@@ -0,0 +1,5 @@
+// Graph that has a vertex for each square on a chessboard ( from 0,0 to 7,7)
+// A field is traversible by the Knight if its either +-1,+-2 or +-2,+-1 away.
+// Examples: From 3,3 -> 
+// 1, 2   | 2, 1    | 1, 4    | 2, 5  | 4, 1    | 5, 2    | 4, 5  | 5, 4
+// -2, -1 | -1, -2  | -2, +1  | -1, +2| +1, -2  | +2, -1  | +1, +2| +2, +1
