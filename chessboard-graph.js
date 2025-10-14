c1d6a8456ef4eaab16b1e96e1d01f8dcf138967d
Michanoku <michael.werker@gmail.com>
2025-10-14 23:42:06 +0000

    Progress on knightmoves

--- chessboard-graph.js
+++ chessboard-graph.js
@@ -120,8 +120,29 @@ class Chessboard {
   knightMoves(start, goal) {
     const startSquare = this.squares[start[0]][start[1]];
     const goalSquare = this.squares[goal[0]][goal[1]];
-    
-    // STUCK HERE
+    const queue = [startSquare];
+    const path = new Array();
+    let found = false;
+    while (queue.length > 0) {
+      if (found === true) {
+        break;
+      }
+      const square = queue.shift();
+      path.push(`[${square.x}, ${square.y}]`);
+      for (const edge of square.edges) {
+        // Add additional checks here
+        if (edge === goalSquare) {
+          console.log("FOUND IT!");
+          found = true;
+          break;
+        } else if (!path.includes(`[${edge.x}, ${edge.y}]`)) {
+          // Calculate the best one to try here for example: which x and y is closest
+          
+          queue.push(edge);
+        };
+      };
+    }
+    return path;
   }
 }
 
